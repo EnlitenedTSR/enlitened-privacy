e6b016d1777bec7aec63c1322bf15e8ff7f1a5ce
Si Mills <si@sjm-create.co.uk>
2019-02-01 16:48:44 +0000

    Add exports to index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.studentPrivacy = require('./student-privacy/index.html');
+exports.studentTerms = require('./student-privacy/index.html');
+exports.universityPrivacy = require('./university-privacy/index.html');
