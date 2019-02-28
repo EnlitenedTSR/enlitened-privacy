4a5cbc03493650bb32c71ffa12e9f49da8ad34bd
Si Mills <si@sjm-create.co.uk>
2019-02-28 16:26:36 +0000

    Update latest and remove student legal content from json

--- build.js
+++ build.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 
-var studentPrivacyIntro = fs.readFileSync('./student_other/your_privacy.html', 'utf8');
-var studentPrivacy = fs.readFileSync('./student_privacy/index.html', 'utf8');
-var studentTerms = fs.readFileSync('./student_terms/index.html', 'utf8');
+// var studentPrivacyIntro = fs.readFileSync('./student_other/your_privacy.html', 'utf8');
+// var studentPrivacy = fs.readFileSync('./student_privacy/index.html', 'utf8');
+// var studentTerms = fs.readFileSync('./student_terms/index.html', 'utf8');
 var universityPrivacy = fs.readFileSync('./university_privacy/index.html', 'utf8');
 
 var data = {
-    studentPrivacyIntro: studentPrivacyIntro.replace(/(\n)(\s)/g, ''),
-    studentPrivacy: studentPrivacy.replace(/(\n)(\s)/g, ''),
-    studentTerms: studentTerms.replace(/(\n)(\s)/g, ''),
+    // studentPrivacyIntro: studentPrivacyIntro.replace(/(\n)(\s)/g, ''),
+    // studentPrivacy: studentPrivacy.replace(/(\n)(\s)/g, ''),
+    // studentTerms: studentTerms.replace(/(\n)(\s)/g, ''),
     universityPrivacy: universityPrivacy.replace(/(\n)(\s)/g, '')
 };
 
